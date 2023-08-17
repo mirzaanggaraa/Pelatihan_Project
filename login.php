048c444437f1b6603454736858c66c898847e84b
mirzaanggaraa <anggaramirza2@gmail.com>
2023-08-17 21:28:25 +0700

    update

---
+++ login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="ceklogin.php" method="post">
+        <input type="text" name="username" id="username"\n>
+        <input type="password" name="password" id="password"\n>
+        <button type="submit">masuk </button>
+    </form>
+</body>
+</html>
